a5bdf6420829f9fef8731b71a9309db39d5a9f9f
arguit <47626763+arguit@users.noreply.github.com>
2021-05-15 22:41:03 +0200

    Update Worker.cs

--- SystemCommandLineHost/Worker.cs
+++ SystemCommandLineHost/Worker.cs
@@ -5,24 +5,24 @@ using Microsoft.Extensions.Logging;
 
 namespace SystemCommandLineHost
 {
-	class Worker : BackgroundService
-	{
-		private readonly ILogger<Worker> logger;
+    class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> logger;
         private readonly Arguments arguments;
 
         public Worker(ILogger<Worker> logger, Arguments arguments)
-		{
-			this.logger = logger;
+        {
+            this.logger = logger;
             this.arguments = arguments;
-		}
+        }
 
-		protected override async Task ExecuteAsync(CancellationToken stoppingToken)
-		{
-			while (!stoppingToken.IsCancellationRequested)
-			{
-				logger.LogInformation($"Configuration:{arguments.Configuration} Publish:{arguments.Publish}");
-				await Task.Delay(1000, stoppingToken);
-			}
-		}
-	}
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                logger.LogInformation($"Configuration:{arguments.Configuration} Publish:{arguments.Publish}");
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
 }
